66fdc1f2fce45ee5257a0b1d2f8b2312368d4c79
Asbjørn Hansen <aha@trustpilot.com>
2015-02-13 13:47:55 +0100

    added armas scraper class and method for getting tab info from the website

--- ArmasSalesTracker/ArmasSalesTracker.cs
+++ ArmasSalesTracker/ArmasSalesTracker.cs
@@ -1,9 +1,35 @@
 ﻿namespace Asser.ArmasSalesTracker
 {
+    using System;
+    using System.Linq;
+    using Asser.ArmasSalesTracker.Services;
+
     public class ArmasSalesTracker
     {
+        private readonly ArmasScraper scraper;
+
+        public ArmasSalesTracker(ArmasScraper scraper)
+        {
+            this.scraper = scraper;
+        }
+
         public static void Main(string[] args)
         {
+            var scraper = new ArmasScraper(new Configuration.Configuration());
+
+            var tracker = new ArmasSalesTracker(scraper);
+
+            tracker.StartTrackingLoop();
+        }
+
+        public void StartTrackingLoop()
+        {
+            var tabLinks = scraper.GetTabs().Skip(1);
+
+            foreach (var tabLink in tabLinks)
+            {
+                Console.WriteLine("Scraping tab: " + tabLink.Title);
+            }
         }
     }
 }
