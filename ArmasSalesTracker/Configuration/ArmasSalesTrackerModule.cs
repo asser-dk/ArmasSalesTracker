22d2a25f51833e7917e9e9295f670c10af0d392e
Asbjørn Hansen <asser-dk@outlook.com>
2015-02-13 18:03:23 +0100

    added ninject module and set up ninject to inject armasscraper

---
+++ ArmasSalesTracker/Configuration/ArmasSalesTrackerModule.cs
@@ -0,0 +1,13 @@
+﻿namespace Asser.ArmasSalesTracker.Configuration
+{
+    using Ninject.Extensions.Conventions;
+    using Ninject.Modules;
+
+    public class ArmasSalesTrackerModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Kernel.Bind(x => x.FromThisAssembly().SelectAllClasses().BindAllInterfaces());
+        }
+    }
+}
