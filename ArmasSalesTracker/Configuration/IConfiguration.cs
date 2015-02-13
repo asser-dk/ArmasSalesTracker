65a1668c36b11b23cb546f4a07bbe5e1da7209d9
Asbjørn Hansen <aha@trustpilot.com>
2015-02-13 13:48:24 +0100

    added configuration for armas page urls

---
+++ ArmasSalesTracker/Configuration/IConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace Asser.ArmasSalesTracker.Configuration
+{
+    public interface IConfiguration
+    {
+        string ArmasBaseHost { get; }
+
+        string ArmasBaseUrl { get; }
+
+        string ArmasFrontpagePageUri { get; }
+    }
+}
