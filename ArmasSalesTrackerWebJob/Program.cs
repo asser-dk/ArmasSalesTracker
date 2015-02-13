06bd15d8b6f78123ceb986e3d8e3852c42ced879
Asbjørn Hansen <asser-dk@outlook.com>
2015-02-13 22:37:24 +0100

    added new webjob to solution

---
+++ ArmasSalesTrackerWebJob/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ArmasSalesTrackerWebJob
+{
+    using Asser.ArmasSalesTracker;
+    using Asser.ArmasSalesTracker.Configuration;
+    using Asser.ArmasSalesTracker.Services;
+    using Microsoft.Azure.WebJobs;
+    using Ninject;
+
+    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
+    public class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        public static void Main()
+        {
+            var host = new JobHost();
+
+            host.Start();
+            var kernel = new StandardKernel(new ArmasSalesTrackerModule());
+
+            var tracker = new ArmasSalesTracker(kernel.Get<IArmasScraper>(), kernel.Get<IProductLineUpdater>());
+
+            tracker.GetLatestData();
+            host.Stop();
+        }
+    }
+}
