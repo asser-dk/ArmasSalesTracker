2f6ab928132bc64f2cb0c18cd6910c3602a3f4f4
Asbjørn Hansen <asser-dk@outlook.com>
2015-02-28 23:13:34 +0100

    removed unused logic from product line service

--- ArmasSalesTracker/Services/ProductLineService.cs
+++ ArmasSalesTracker/Services/ProductLineService.cs
@@ -14,19 +14,11 @@
 
         private readonly MySqlCommand updateProductCommand;
 
-        private readonly MySqlCommand latestPricePointCommand;
-
-        private readonly MySqlCommand insertPricePointCommand;
-
-        private readonly MySqlCommand updatePricePointTimestampCommand;
-
-        private readonly MySqlCommand insertProductPriceCommand;
-
-        private readonly MySqlCommand normalPriceCommand;
-
         private readonly MySqlCommand latestPriceCommand;
 
-        private readonly MySqlCommand updateProductPriceCommand;
+        private readonly MySqlCommand insertPriceCommand;
+
+        private readonly MySqlCommand updatePriceTimestampCommand;
 
         public ProductLineService(IConfiguration configuration)
         {
@@ -40,81 +32,32 @@
                     + "ON DUPLICATE KEY UPDATE Url=VALUES(Url), ImageUrl=VALUES(ImageUrl), Title=VALUES(Title), Category=VALUES(Category)",
                     connection);
 
-            latestPricePointCommand =
+            latestPriceCommand =
                 new MySqlCommand(
                     "SELECT Timestamp, Value FROM Price WHERE Product = @ProductId AND Type = @Type ORDER BY Timestamp DESC LIMIT 1",
                     connection);
 
-            insertPricePointCommand = new MySqlCommand("INSERT INTO Price (Product, Value, Type, Timestamp) VALUES (@ProductId, @Value, @Type, @Timestamp)", connection);
+            insertPriceCommand = new MySqlCommand("INSERT INTO Price (Product, Value, Type, Timestamp) VALUES (@ProductId, @Value, @Type, @Timestamp)", connection);
 
-            updatePricePointTimestampCommand =
+            updatePriceTimestampCommand =
                 new MySqlCommand(
                     "UPDATE Price SET Timestamp = NOW() WHERE Product = @ProductId AND Type = @Type AND @Timestamp = @Timestamp AND Value = @Value LIMIT 1",
                     connection);
 
-            insertProductPriceCommand = new MySqlCommand(
-                "INSERT INTO ProductPrice (Product, Price, PremiumPrice, Timestamp, DefaultPrice) VALUES (@Product, @Price, @PremiumPrice, @Timestamp, @DefaultPrice)",
-                connection);
-            normalPriceCommand =
-                new MySqlCommand(
-                    "SELECT Price, PremiumPrice, DefaultPrice from ProductPrice WHERE Product = @ProductId ORDER BY Timestamp DESC LIMIT 1",
-                    connection);
-            latestPriceCommand = new MySqlCommand("SELECT Price, PremiumPrice, DefaultPrice, Timestamp FROM ProductPrice WHERE Product = @ProductId ORDER BY Timestamp DESC LIMIT 1", connection);
-            updateProductPriceCommand = new MySqlCommand("UPDATE ProductPrice SET Timestamp = NOW() WHERE Product = @ProductId AND Timestamp = @Timestamp LIMIT 1", connection);
-
             updateProductCommand.Prepare();
-            insertProductPriceCommand.Prepare();
-            normalPriceCommand.Prepare();
-            updateProductPriceCommand.Prepare();
             latestPriceCommand.Prepare();
+            insertPriceCommand.Prepare();
+            updatePriceTimestampCommand.Prepare();
         }
 
-        public void InsertProductPrice(ProductLine productLine)
+        public void Dispose()
         {
-            insertProductPriceCommand.Parameters.Clear();
-            insertProductPriceCommand.Parameters.AddWithValue("@Product", productLine.Id);
-            insertProductPriceCommand.Parameters.AddWithValue("@Price", productLine.Prices.Price);
-            insertProductPriceCommand.Parameters.AddWithValue("@PremiumPrice", productLine.Prices.Premium);
-            insertProductPriceCommand.Parameters.AddWithValue("@DefaultPrice", productLine.Prices.DefaultPrice);
-            insertProductPriceCommand.ExecuteNonQuery();
-        }
-
-        public void UpdateLatestPrice(ProductLine productLine)
-        {
-            latestPriceCommand.Parameters.Clear();
-            latestPriceCommand.Parameters.AddWithValue("@ProductId", productLine.Id);
-
-            var reader = latestPriceCommand.ExecuteReader();
-            ProductPrice latest = null;
-            if (reader.HasRows)
-            {
-                reader.Read();
-                latest = new ProductPrice
-                {
-                    Price = reader.GetInt32("Price"),
-                    Premium = reader.GetInt32("PremiumPrice"),
-                    DefaultPrice = reader.GetInt32("DefaultPrice"),
-                    Timestamp = reader.GetDateTime("Timestamp")
-                };
-            }
-
-            reader.Close();
-
-            if (latest != null && productLine.Prices.Equals(latest))
-            {
-                Log.Debug("Same prices, updating old record with new timestamp");
-
-                updateProductPriceCommand.Parameters.Clear();
-                updateProductPriceCommand.Parameters.AddWithValue("@ProductId", productLine.Id);
-                updateProductPriceCommand.Parameters.AddWithValue("@Timestamp", latest.Timestamp);
-                updateProductPriceCommand.ExecuteNonQuery();
-            }
-            else
-            {
-                Log.Debug("Inserting new product price line");
-
-                InsertProductPrice(productLine);
-            }
+            updateProductCommand.Dispose();
+            latestPriceCommand.Dispose();
+            insertPriceCommand.Dispose();
+            updateProductCommand.Dispose();
+            connection.Close();
+            connection.Dispose();
         }
 
         public void UpdateProductData(Product productLine)
@@ -143,33 +86,13 @@
             }
         }
 
-        private void UpdatePricePointTimestamp(string productId, Price price)
-        {
-            updatePricePointTimestampCommand.Parameters.Clear();
-            updatePricePointTimestampCommand.Parameters.AddWithValue("@ProductId", productId);
-            updatePricePointTimestampCommand.Parameters.AddWithValue("@Value", price.Value);
-            updatePricePointTimestampCommand.Parameters.AddWithValue("@Type", (int)price.Type);
-            updatePricePointTimestampCommand.Parameters.AddWithValue("@Timestamp", price.Timestamp);
-            updatePricePointTimestampCommand.ExecuteNonQuery();
-        }
-
-        private void InsertPricePoint(string productId, Price price)
-        {
-            insertPricePointCommand.Parameters.Clear();
-            insertPricePointCommand.Parameters.AddWithValue("@ProductId", productId);
-            insertPricePointCommand.Parameters.AddWithValue("@Value", price.Value);
-            insertPricePointCommand.Parameters.AddWithValue("@Type", (int)price.Type);
-            insertPricePointCommand.Parameters.AddWithValue("@Timestamp", price.Timestamp);
-            insertPricePointCommand.ExecuteNonQuery();
-        }
-
         public Price GetLatestPrice(string productId, PriceTypes type)
         {
-            latestPricePointCommand.Parameters.Clear();
-            latestPricePointCommand.Parameters.AddWithValue("@ProductId", productId);
-            latestPricePointCommand.Parameters.AddWithValue("@Type", (int)type);
+            latestPriceCommand.Parameters.Clear();
+            latestPriceCommand.Parameters.AddWithValue("@ProductId", productId);
+            latestPriceCommand.Parameters.AddWithValue("@Type", (int)type);
 
-            using (var reader = latestPricePointCommand.ExecuteReader())
+            using (var reader = latestPriceCommand.ExecuteReader())
             {
                 return reader.Read()
                            ? new Price
@@ -182,40 +105,24 @@
             }
         }
 
-        public ProductPrice GetNormalPrices(string productId)
+        private void UpdatePricePointTimestamp(string productId, Price price)
         {
-            try
-            {
-                normalPriceCommand.Parameters.Clear();
-                normalPriceCommand.Parameters.AddWithValue("@ProductId", productId);
-                var results = normalPriceCommand.ExecuteReader();
-                results.Read();
-
-                var productPrice = new ProductPrice
-                {
-                    Price = results.GetInt32("Price"),
-                    Premium = results.GetInt32("PremiumPrice"),
-                    DefaultPrice = results.GetInt32("DefaultPrice")
-                };
-
-                results.Close();
-
-                return productPrice;
-            }
-            catch (MySqlException ex)
-            {
-                Log.Error(string.Format("Error trying to get normal price for product {0}", productId), ex);
-
-                throw;
-            }
+            updatePriceTimestampCommand.Parameters.Clear();
+            updatePriceTimestampCommand.Parameters.AddWithValue("@ProductId", productId);
+            updatePriceTimestampCommand.Parameters.AddWithValue("@Value", price.Value);
+            updatePriceTimestampCommand.Parameters.AddWithValue("@Type", (int)price.Type);
+            updatePriceTimestampCommand.Parameters.AddWithValue("@Timestamp", price.Timestamp);
+            updatePriceTimestampCommand.ExecuteNonQuery();
         }
 
-        public void Dispose()
+        private void InsertPricePoint(string productId, Price price)
         {
-            insertProductPriceCommand.Dispose();
-            updateProductCommand.Dispose();
-            connection.Close();
-            connection.Dispose();
+            insertPriceCommand.Parameters.Clear();
+            insertPriceCommand.Parameters.AddWithValue("@ProductId", productId);
+            insertPriceCommand.Parameters.AddWithValue("@Value", price.Value);
+            insertPriceCommand.Parameters.AddWithValue("@Type", (int)price.Type);
+            insertPriceCommand.Parameters.AddWithValue("@Timestamp", price.Timestamp);
+            insertPriceCommand.ExecuteNonQuery();
         }
     }
 }
