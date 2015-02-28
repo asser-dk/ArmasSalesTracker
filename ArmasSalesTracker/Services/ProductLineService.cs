3d34dd5b827fe9873f981d25e260b7ea4decf9d3
Asbjørn Hansen <asser-dk@outlook.com>
2015-02-28 23:27:35 +0100

    moved price logic into new service class

--- ArmasSalesTracker/Services/ProductLineService.cs
+++ ArmasSalesTracker/Services/ProductLineService.cs
@@ -14,12 +14,6 @@
 
         private readonly MySqlCommand updateProductCommand;
 
-        private readonly MySqlCommand latestPriceCommand;
-
-        private readonly MySqlCommand insertPriceCommand;
-
-        private readonly MySqlCommand updatePriceTimestampCommand;
-
         public ProductLineService(IConfiguration configuration)
         {
             connection = new MySqlConnection(configuration.MySqlConnectionString);
@@ -32,29 +26,11 @@
                     + "ON DUPLICATE KEY UPDATE Url=VALUES(Url), ImageUrl=VALUES(ImageUrl), Title=VALUES(Title), Category=VALUES(Category)",
                     connection);
 
-            latestPriceCommand =
-                new MySqlCommand(
-                    "SELECT Timestamp, Value FROM Price WHERE Product = @ProductId AND Type = @Type ORDER BY Timestamp DESC LIMIT 1",
-                    connection);
-
-            insertPriceCommand = new MySqlCommand("INSERT INTO Price (Product, Value, Type, Timestamp) VALUES (@ProductId, @Value, @Type, @Timestamp)", connection);
-
-            updatePriceTimestampCommand =
-                new MySqlCommand(
-                    "UPDATE Price SET Timestamp = NOW() WHERE Product = @ProductId AND Type = @Type AND @Timestamp = @Timestamp AND Value = @Value LIMIT 1",
-                    connection);
-
             updateProductCommand.Prepare();
-            latestPriceCommand.Prepare();
-            insertPriceCommand.Prepare();
-            updatePriceTimestampCommand.Prepare();
         }
 
         public void Dispose()
         {
-            updateProductCommand.Dispose();
-            latestPriceCommand.Dispose();
-            insertPriceCommand.Dispose();
             updateProductCommand.Dispose();
             connection.Close();
             connection.Dispose();
@@ -70,59 +46,6 @@
             updateProductCommand.Parameters.AddWithValue("@Title", productLine.Title);
             updateProductCommand.Parameters.AddWithValue("@Category", productLine.Category);
             updateProductCommand.ExecuteNonQuery();
-        }
-
-        public void UpdatePriceInfo(string productId, Price price)
-        {
-            var latestPrice = GetLatestPrice(productId, price.Type);
-
-            if (latestPrice.Value != price.Value)
-            {
-                InsertPricePoint(productId, price);
-            }
-            else
-            {
-                UpdatePricePointTimestamp(productId, latestPrice);
-            }
-        }
-
-        public Price GetLatestPrice(string productId, PriceTypes type)
-        {
-            latestPriceCommand.Parameters.Clear();
-            latestPriceCommand.Parameters.AddWithValue("@ProductId", productId);
-            latestPriceCommand.Parameters.AddWithValue("@Type", (int)type);
-
-            using (var reader = latestPriceCommand.ExecuteReader())
-            {
-                return reader.Read()
-                           ? new Price
-                           {
-                               Timestamp = reader.GetDateTime("Timestamp"),
-                               Value = reader.GetInt32("Value"),
-                               Type = type
-                           }
-                           : null;
-            }
-        }
-
-        private void UpdatePricePointTimestamp(string productId, Price price)
-        {
-            updatePriceTimestampCommand.Parameters.Clear();
-            updatePriceTimestampCommand.Parameters.AddWithValue("@ProductId", productId);
-            updatePriceTimestampCommand.Parameters.AddWithValue("@Value", price.Value);
-            updatePriceTimestampCommand.Parameters.AddWithValue("@Type", (int)price.Type);
-            updatePriceTimestampCommand.Parameters.AddWithValue("@Timestamp", price.Timestamp);
-            updatePriceTimestampCommand.ExecuteNonQuery();
-        }
-
-        private void InsertPricePoint(string productId, Price price)
-        {
-            insertPriceCommand.Parameters.Clear();
-            insertPriceCommand.Parameters.AddWithValue("@ProductId", productId);
-            insertPriceCommand.Parameters.AddWithValue("@Value", price.Value);
-            insertPriceCommand.Parameters.AddWithValue("@Type", (int)price.Type);
-            insertPriceCommand.Parameters.AddWithValue("@Timestamp", price.Timestamp);
-            insertPriceCommand.ExecuteNonQuery();
         }
     }
 }
