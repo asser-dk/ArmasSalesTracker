4a5c73de18d0f06503e7ee89f0f6b814aa9eeb15
Asbjørn Hansen <asser-dk@outlook.com>
2015-02-28 22:18:38 +0100

    added a new product class to replace the product line class

---
+++ ArmasSalesTracker/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace Asser.ArmasSalesTracker.Models
+{
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+
+    public class Product
+    {
+        public Product()
+        {
+            PriceInfo = new Collection<Price>();
+        }
+
+        public string Id { get; set; }
+
+        public string Url { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Title { get; set; }
+
+        public string Category { get; set; }
+
+        public ICollection<Price> PriceInfo { get; set; }
+    }
+}
