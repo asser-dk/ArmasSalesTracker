f756a062de6dc8b6ebb1d0355d81ca296b791bc5
Asbjørn Hansen <asser-dk@outlook.com>
2015-02-28 21:39:03 +0100

    added missing user agent to get sub pages request

--- ArmasSalesTracker/Services/ArmasScraper.cs
+++ ArmasSalesTracker/Services/ArmasScraper.cs
@@ -226,6 +226,7 @@
 
                 var redirectRequest = (HttpWebRequest)WebRequest.Create(response.ResponseUri);
                 redirectRequest.CookieContainer = GetCookies();
+                redirectRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36";
 
                 using (var redirectResponse = redirectRequest.GetResponse())
                 {
