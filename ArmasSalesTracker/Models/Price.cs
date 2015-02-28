b9d194022cee54af7e2464344117bd03e1ef8aa9
Asbjørn Hansen <asser-dk@outlook.com>
2015-02-28 21:52:22 +0100

    added price model to match new database model

---
+++ ArmasSalesTracker/Models/Price.cs
@@ -0,0 +1,13 @@
+﻿namespace Asser.ArmasSalesTracker.Models
+{
+    using System;
+
+    public class Price
+    {
+        public DateTime Timestamp { get; set; }
+
+        public PriceTypes Type { get; set; }
+
+        public int Value { get; set; }
+    }
+}
---
+++ ArmasSalesTracker/Models/PriceTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace Asser.ArmasSalesTracker.Models
+{
+    public enum PriceTypes
+    {
+        Default = 1,
+
+        Discount = 2,
+
+        Premium = 3
+    }
+}
